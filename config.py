cdb0fbc7c16d483edc1a274c58eb297d4a2f7c68
snroboticslabhead-pixel <snroboticslabhead@gmail.com>
2025-11-18 14:50:41 +0530

    Refactor MySQL configuration in config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-2024'
     
     # MySQL configuration for PythonAnywhere
-    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'kosinventory.mysql.pythonanywhere-services.com'
-    MYSQL_USER = os.environ.get('MYSQL_USER') or 'kosinventory'
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'Labkos@532'
-    MYSQL_DB = os.environ.get('MYSQL_DB') or 'kosinventory$default'
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'inventorykos.mysql.pythonanywhere-services.com'
+    MYSQL_USER = os.environ.get('MYSQL_USER') or 'inventorykos'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'Krishna@532'
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'inventorykos$default'
     
     DEBUG = False
     TESTING = False
@@ -25,4 +25,5 @@ config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
     'default': DevelopmentConfig
+
 }
